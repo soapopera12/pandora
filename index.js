1b8598bd7bfcc506a5b7bd3839c1ddf2f556ace8
Nikhil Gumasthi <teslappop9090@gmail.com>
2024-01-18 12:34:05 +0530

    added content

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 
 window.onload = function() {
 
+const mtechta = document.getElementById("experience-accordion-mtechta");
 const mtech = document.getElementById("experience-accordion-mtech");
 const btech = document.getElementById("experience-accordion-btech");
 const enkinlab = document.getElementById("experience-accordion-enkinlab");
 const school = document.getElementById("experience-accordion-school");
 
+const contentmtechta = document.getElementById("experience-accordion-content-mtechta");
 const contetmtech = document.getElementById("experience-accordion-content-mtech");
 const contettbtech = document.getElementById("experience-accordion-content-btech");
 const contetenkinlab = document.getElementById("experience-accordion-content-enkinlab");
 const contetschool = document.getElementById("experience-accordion-content-school");
 
 
+mtechta.addEventListener("click", function() {
+    contentmtechta.style.display = "block";
+    contetmtech.style.display = "none";
+    contettbtech.style.display = "none";
+    contetenkinlab.style.display = "none";
+    contetschool.style.display = "none";
+    mtechta.style.color = "dodgerblue";
+    mtech.style.color = "whitesmoke";
+    btech.style.color = "whitesmoke";
+    enkinlab.style.color = "whitesmoke";
+    school.style.color = "whitesmoke";
+});
+
 mtech.addEventListener("click", function() {
+    contentmtechta.style.display = "none";
     contetmtech.style.display = "block";
     contettbtech.style.display = "none";
     contetenkinlab.style.display = "none";
     contetschool.style.display = "none";
+    mtechta.style.color = "whitesmoke";
     mtech.style.color = "dodgerblue";
     btech.style.color = "whitesmoke";
     enkinlab.style.color = "whitesmoke";
@@ -24,10 +41,12 @@ mtech.addEventListener("click", function() {
 });
 
 btech.addEventListener("click", function() {
+    contentmtechta.style.display = "none";
     contettbtech.style.display = "block";
     contetmtech.style.display = "none";
     contetenkinlab.style.display = "none";
     contetschool.style.display = "none";
+    mtechta.style.color = "whitesmoke";
     mtech.style.color = "whitesmoke";
     btech.style.color = "dodgerblue";
     enkinlab.style.color = "whitesmoke";
@@ -35,10 +54,12 @@ btech.addEventListener("click", function() {
 });
 
 enkinlab.addEventListener("click", function() {
+    contentmtechta.style.display = "none";
     contetenkinlab.style.display = "block";
     contettbtech.style.display = "none";
     contetmtech.style.display = "none";
     contetschool.style.display = "none";
+    mtechta.style.color = "whitesmoke";
     mtech.style.color = "whitesmoke";
     btech.style.color = "whitesmoke";
     enkinlab.style.color = "dodgerblue";
@@ -46,10 +67,12 @@ enkinlab.addEventListener("click", function() {
 });
 
 school.addEventListener("click", function() {
+    contentmtechta.style.display = "none";
     contetschool.style.display = "block";
     contettbtech.style.display = "none";
     contetenkinlab.style.display = "none";
     contetmtech.style.display = "none";
+    mtechta.style.color = "whitesmoke";
     mtech.style.color = "whitesmoke";
     btech.style.color = "whitesmoke";
     enkinlab.style.color = "whitesmoke";
